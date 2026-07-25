6c48ab78899aa4aa15eebda311a3bc0a202161a3
mofmango <76798604+mofmango@users.noreply.github.com>
2026-07-25 11:01:57 +0900

    Delete backend/projectB directory

--- backend/projectB/src/main/java/ce/mnu/projectB/repository/GenreRepository.java
+++
@@ -1,7 +0,0 @@
-package ce.mnu.projectB.repository;
-
-import ce.mnu.projectB.domain.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenreRepository extends JpaRepository<Genre, Long> {
-}
